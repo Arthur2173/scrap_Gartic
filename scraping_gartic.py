5bb289fc6fe597d05791b178061c2468824fbd80
Arthur2173 <fontouraarthur518@gmail.com>
2025-12-30 21:26:01 -0500

    mundando nome das variaveis

--- scraping_gartic.py
+++ scraping_gartic.py
@@ -5,25 +5,25 @@ from bs4 import BeautifulSoup
 
 #url do site
 url ="https://respostadogartic.blogspot.com/2017/10/lista-de-alimentos-gartic.html?m=1"
-resposta = requests.get(url)
+response = requests.get(url)
 
 #pegando todo o html da pagina
-soup = BeautifulSoup(resposta.text, 'html.parser')
+soup = BeautifulSoup(response.text, 'html.parser')
 
 #pegando os dados da pagina usando find_all() os dados estao na tag button
-palavras = soup.find_all("button")
+words = soup.find_all("button")
 
-tamanho = int(input("informe o tamanho da dica"))
+item_count = int(input("lenght: "))
  
-for palavra in palavras:
+for word in words:
      #tratando as palavras usando replace,strip(). usando o replace para tirar os espaços do meio das palavras pois
      #acaba atrapalhando na hora de ver o tamanho delas
-     tratamento = palavra.text.strip().replace(" ","")
+     treatment = word.text.strip().replace(" ","")
      
-     texto = palavra.text.strip()
+     texto = word.text.strip()
      
      #agora as palavras estão sendo separadas por tamanho ajuda muito na hora que alguem coloca as dicas no jogo
-     if len(tratamento) == tamanho:
+     if len(treatment) == item_count:
          print(texto)
          
          
